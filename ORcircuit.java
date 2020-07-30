1aa5db4951cfdda940bb691efef545101b284b9f
umamina11 <62588358+umamina11@users.noreply.github.com>
2020-07-30 20:55:13 +0530

    OR SHORTCIRCUIT
    write a program to demonstrate the use of OR short circit using simple program

---
+++ ORcircuit.java
@@ -0,0 +1,22 @@
+//write a program to demonstrate the use of short cicuit OR
+package demo;
+
+
+public class ORcircuit 
+{
+      public static void main(String arg[]) 
+    { 
+        if (2 + 2 == 4 || 0 == 1) 
+        {
+            System.out.println(" is true");
+        }
+        else 
+        {
+            System.out.println(" is false");
+        }
+
+       System.out.println();
+    }
+} 
+    
+
