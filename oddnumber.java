64a753b30f01ff2ffffdf5d78a5e3706e41ef7f6
umamina11 <62588358+umamina11@users.noreply.github.com>
2020-08-22 21:32:27 +0530

    odd numbers
    printing all odd numbers between  1 to 100 using continue

---
+++ oddnumber.java
@@ -0,0 +1,28 @@
+
+package demo;
+
+
+public class oddnumber {
+    public static void main(String args[])
+    {
+
+         
+        int i,num;
+        System.out.println("0dd number between 1 to 100 are::");
+        for(num=1;num<=100;num++)
+        {
+            
+      
+            if(num % 2!=0)
+            {
+                
+                System.out.print(num+" ");
+                continue;     
+            }
+        
+       
+    }
+    }
+    
+}
+
